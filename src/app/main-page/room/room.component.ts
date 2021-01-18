ad9c0ec19903907b1966bdb888bd866d6fb4d5b1
raisusmita <susmitathulung53@gmail.com>
2021-01-18 18:42:43 +0545

    room skeleton completed

--- src/app/main-page/room/room.component.ts
+++ src/app/main-page/room/room.component.ts
@@ -7,9 +7,30 @@ import { CheckRoomService } from "src/app/home/check-room.service";
   styleUrls: ["./room.component.scss"],
 })
 export class RoomComponent implements OnInit {
+  nearLocation: any[] = [
+    {
+      icon: "environment",
+      location: "Charumati Stupa",
+      distance: "15 min drive",
+    },
+    { icon: "environment", location: "Boudhanath", distance: "16 min drive" },
+    {
+      icon: "environment",
+      location: "Riwoche Monastery",
+      distance: "18 min drive",
+    },
+    {
+      icon: "environment",
+      location: "Kathmandu (KTM-Tribhuvan Intl.)",
+      distance: "9 min drive",
+    },
+  ];
+
+  test: any;
   constructor(private roomCheckService: CheckRoomService) {}
 
   ngOnInit() {
+    this.test = this.roomCheckService.checkInOutDate;
     console.log("from room" + this.roomCheckService.checkInOutDate);
   }
 }
