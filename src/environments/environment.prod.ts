348431d62b43c4983471cdb1ce40a1d5aef7bd4f
raisusmita <susmitathulung53@gmail.com>
2021-03-03 20:07:57 +0545

    fix build error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: "https://karnalihome.com/api/",
-  // serverURL: "https://karnalihome.com/api/",
+  serverURL: "https://karnalihome.com/api/",
 };
